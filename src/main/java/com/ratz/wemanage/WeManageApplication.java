b8bb064c3bbb36a9a32bb02c48e3726bda66e778
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2023-10-13 00:43:47 +0000

    fixed bug with bcrypt bean. hidded schema sql

--- src/main/java/com/ratz/wemanage/WeManageApplication.java
+++ src/main/java/com/ratz/wemanage/WeManageApplication.java
@@ -4,18 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class WeManageApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WeManageApplication.class, args);
-	}
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WeManageApplication.class, args);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        int BCRYPT_STRENGTH = 12;
+        return new BCryptPasswordEncoder(BCRYPT_STRENGTH);
+    }
 
 
 }
