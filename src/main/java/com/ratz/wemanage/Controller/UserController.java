cb129316ec6010fc4719b0e3ec04c3e3560c31cb
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2023-10-13 00:24:32 +0000

    created http response, controler and create user endpoint

---
+++ src/main/java/com/ratz/wemanage/Controller/UserController.java
@@ -0,0 +1,47 @@
+package com.ratz.wemanage.Controller;
+
+import com.ratz.wemanage.domain.User;
+import com.ratz.wemanage.domain.response.HttpResponse;
+import com.ratz.wemanage.dto.UserDTO;
+import com.ratz.wemanage.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.Map;
+
+import static java.time.LocalDateTime.now;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1")
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/user")
+    public ResponseEntity<HttpResponse> createUser(@RequestBody @Valid User user) {
+
+        UserDTO userDTO = userService.createUser(user);
+
+        return ResponseEntity.created(getURI())
+                .body(HttpResponse.builder()
+                        .timeStamp(now().toString())
+                        .data(Map.of("user", userDTO))
+                        .message("User created")
+                        .statusCode(HttpStatus.CREATED.value())
+                        .httpStatus(HttpStatus.CREATED)
+                        .build());
+    }
+
+    private URI getURI() {
+        return URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/v1/user/get/<userId>").toUriString());
+    }
+}
--- src/main/java/com/ratz/wemanage/service/impl/UserServiceImpl.java
+++ src/main/java/com/ratz/wemanage/service/impl/UserServiceImpl.java
@@ -2,13 +2,22 @@ package com.ratz.wemanage.service.impl;
 
 import com.ratz.wemanage.domain.User;
 import com.ratz.wemanage.dto.UserDTO;
+import com.ratz.wemanage.mapper.UserDTOMapper;
+import com.ratz.wemanage.repository.UserRepository;
 import com.ratz.wemanage.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
+@Service
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository<User> userRepository;
 
     @Override
     public UserDTO createUser(User user) {
-        return null;
+
+        return UserDTOMapper.fromUser(userRepository.create(user));
     }
 }
 
